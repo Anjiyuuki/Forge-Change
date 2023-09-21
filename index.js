b27557ca6e1c6a4ddc98d5f8cef1b2e3379803da
Julia Kempton <kemptonjq@g.cofc.edu>
2023-09-21 17:52:22 -0400

    Reorganized pages'

--- index.js
+++ index.js
@@ -1,23 +1,2 @@
 // JavaScript code for handling navigation
 
-
-document.getElementById('explore').addEventListener('click', function () {
-  // Handle the "Explore" click event
-  alert('Explore clicked'); // Replace with your logic
-});
-
-document.getElementById('groups').addEventListener('click', function () {
-  // Handle the "Groups" click event
-  alert('Groups clicked'); // Replace with your logic
-});
-
-document.getElementById('inbox').addEventListener('click', function () {
-  // Handle the "Inbox" click event
-  alert('Inbox clicked'); // Replace with your logic
-});
-
-document.getElementById('info').addEventListener('click', function () {
-  // Handle the "Info" click event
-  alert('Info clicked'); // Replace with your logic
-});
-
