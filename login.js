d59e4b936bef5a792f55be325a5b5e6b75fabfcd
Julia Kempton <kemptonjq@g.cofc.edu>
2023-10-18 20:28:37 -0400

    Added register boxes

--- login.js
+++ login.js
@@ -22,3 +22,43 @@ loginForm.addEventListener("submit", (e) => {
             errorMessage.textContent = error.message;
         });
 });
+
+
+// Reference the registration form and error message
+const registerForm = document.getElementById("register-form");
+const registerEmailInput = document.getElementById("register-email");
+const registerPasswordInput = document.getElementById("register-password");
+const registerErrorMessage = document.getElementById("register-error-message");
+
+registerForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = registerEmailInput.value;
+    const password = registerPasswordInput.value;
+
+    // Register a new user with email and password
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Registration successful
+            const user = userCredential.user;
+
+            // Add user data to Firestore
+            const db = firebase.firestore();
+            db.collection("users").doc(user.uid).set({
+                email: user.email,
+                // Add additional user data here if needed
+            })
+            .then(() => {
+                // User data added to Firestore, you can redirect the user to the login page
+                window.location.href = "login.html";
+            })
+            .catch((error) => {
+                // Handle Firestore data addition errors and display error message
+                registerErrorMessage.textContent = error.message;
+            });
+        })
+        .catch((error) => {
+            // Handle registration errors and display error message
+            registerErrorMessage.textContent = error.message;
+        });
+});
+
