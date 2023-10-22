0c27d4fd4e24227cdb1364940fc752df4c3a9be7
Julia Kempton <kemptonjq@g.cofc.edu>
2023-10-22 12:54:02 -0400

    Added questions to registration form, enhanced profile

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// Your web app's Firebase configuration
+// Reference to Firebase Authentication
+var auth = firebase.auth();
+// Reference to Firebase Realtime Database
+var database = firebase.database();
+
 var firebaseConfig = {
   apiKey: "AIzaSyBqQI5B3nOy4tI1GdgVZpcwFsebQhOeVrM",
   authDomain: "forge-change.firebaseapp.com",
@@ -9,17 +13,12 @@ var firebaseConfig = {
   appId: "1:325085017096:web:6cf889cd7e5bd75e088dbd",
   measurementId: "G-9Z8QFCGL1Q"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const database = firebase.database()
-
-// Reference to Firebase Authentication
-var auth = firebase.auth();
 
 document.getElementById('registerButton').addEventListener('click', function() {
     var email = document.getElementById('regEmail').value;
     var password = document.getElementById('regPassword').value;
+    var interests = document.getElementById('regInterests').value;
+    var location = document.getElementById('regLocation').value;
 
     // Create a new user with email and password
     auth.createUserWithEmailAndPassword(email, password)
@@ -27,9 +26,29 @@ document.getElementById('registerButton').addEventListener('click', function() {
             // User registered successfully
             var user = userCredential.user;
             document.getElementById('message').textContent = 'Registration successful. User UID: ' + user.uid;
+            firebase.auth().onAuthStateChanged(function(user) {
+              if (user) {
+                  // User is signed in. You can now write data to the database.
+                  // Store additional user data (interests and location) in the Realtime Database
+                database.ref('users/' + user.uid).set({
+                interests: interests,
+                location: location
+          })
+          .then(function() {
+            console.log('Data write succeeded');
+          })
+          .catch(function(error) {
+              console.error('Data write failed: ' + error.message);
+          });
+              } else {
+                  // User is not signed in. Handle authentication.
+                  console.log("error");
+              }
+          });
+            
 
             // Redirect to explore.html
-            window.location.href = 'explore.html';
+            //window.location.href = 'explore.html';
         })
         .catch(function(error) {
             // Handle errors during registration
@@ -51,7 +70,7 @@ document.getElementById('loginButton').addEventListener('click', function() {
             document.getElementById('message').textContent = 'Login successful. User UID: ' + user.uid;
 
             // Redirect to explore.html
-            window.location.href = 'explore.html';
+            //window.location.href = 'explore.html';
         })
         .catch(function(error) {
             // Handle errors during login
